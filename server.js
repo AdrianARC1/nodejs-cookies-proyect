8fd8114d0c07546c58ac768f5a8f8f255f9e8235
Adrián Ramón <adrianramon2003@gmail.com>
2023-01-31 09:53:06 +0100

    Probadas diferentes maneras de modificar las cookies

--- server.js
+++ server.js
@@ -12,11 +12,24 @@ app.get('/setcookie', (req, res)=>{
     res.cookie('nombre de la cookie', 'cookie', {
         // maxAge: 10000,
         expires: new Date('2023-12-31'),
-        httpOnly: true
+        httpOnly: true,
+        secure: true,
+        sameSite: 'lax'
     })
+
     res.send("Pagina principal del manejo de COOKIES")
 })
 
+app.get('/getcookie', (req, res)=>{
+    console.log(req.cookies)
+    res.send("Leyendo cookies...")
+})
+
+app.get('/deletecookie', (req, res)=>{
+    console.log(req.cookies)
+    res.clearCookie('nombre de la cookie')
+    res.send("Eliminando cookies...")
+})
 
 app.listen(3000)
 console.log('Server on port 3000')
